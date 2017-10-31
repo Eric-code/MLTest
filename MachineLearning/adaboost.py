891c234a2b41a753163bf1483d17423d6f577857
hebo <hebo@gmail.com>
2017-10-31 10:52:58 +0800

    机器学习实战分类部分结束

---
+++ MachineLearning/adaboost.py
@@ -0,0 +1,12 @@
+from numpy import matrix
+
+
+def loadSimpData():
+    dataMat = matrix([[1., 2.1], [2., 1.1], [1.3, 1.], [1., 1.], [2., 1.]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return dataMat, classLabels
+
+
+if __name__ == '__main__':
+    datMat, classLabels = loadSimpData()
+
